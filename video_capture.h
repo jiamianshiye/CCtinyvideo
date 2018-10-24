cb7132381a6a372fbdf674d88c84e7eddcc26071
cdp <chengdapeng@penguinsinnovate.com>
2018-10-24 16:40:59 +0800

    1.Fix fatal msg!

--- video_capture.h
+++ video_capture.h
@@ -1,6 +1,25 @@
-#ifndef DRAW_FRAMEBUFFER_H_
-#define DRAW_FRAMEBUFFER_H_
-void init_framebuffer();
-void draw_framebuffer(unsigned char* src);
-void free_framebuffer();
-#endif /* DRAW_FRAMEBUFFER_H_ */
+/*
+ * video_capture.h
+ *
+ *  Created on: Dec 24, 2015
+ *      Author: Lincoln
+ */
+
+#ifndef VIDEO_CAPTURE_H_
+#define VIDEO_CAPTURE_H_
+
+/* check the supported webcam resolutions using $v4l2-ctl --list-formats-ext */
+#define IM_WIDTH 640
+#define IM_HEIGHT 480
+#define CLEAR(x) memset (&(x), 0, sizeof (x))
+
+struct buffer {
+	void * start;
+	size_t length;
+};
+
+void init_video_capture();
+char video_capture(unsigned char* dst);
+void free_video_capture();
+
+#endif /* VIDEO_CAPTURE_H_ */
